5ea72d45b2397022066e1cf75ea11c478a618d47
EugeneParallax <gardik87@gmail.com>
2024-04-14 22:31:58 +0300

    Initial commit

---
+++ src/main/java/ru/skyproera/springbootcalchw24/SpringBootCalchw24Application.java
@@ -0,0 +1,13 @@
+package ru.skyproera.springbootcalchw24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCalchw24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCalchw24Application.class, args);
+	}
+
+}
